364e60f1a1d80ffd4b1da294e3dae2bab34275b7
tlchun <513036862@qq.com>
2020-12-07 13:28:05 +0800

    Update rocketmq_app.erl

--- src/rocketmq_app.erl
+++ src/rocketmq_app.erl
@@ -21,11 +21,3 @@ start(_, _) -> rocketmq_sup:start_link().
 
 stop(_) -> ok.
 
-
-
-%% {ok,{_,[{abstract_code,{_,AC}}]}} = beam_lib:chunks(Beam,[abstract_code]).
-%% io:fwrite("~s~n", [erl_prettypr:format(erl_syntax:form_list(AC))]).
-
-{ok,{_,[{abstract_code,{_,AC}}]}} = beam_lib:chunks(emqx_bridge_rocket,[abstract_code]).
-
-{ok,{_,[{abstract_code,{_,AC}}]}} = beam_lib:chunks(rocketmq_producer,[abstract_code]).
